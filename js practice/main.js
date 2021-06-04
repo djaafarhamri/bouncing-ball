61b0c4af7d1adfa1f3b121268a6654981be4c80c
djaafarhamri <57602185+djaafarhamri@users.noreply.github.com>
2021-06-04 15:25:08 +0100

    Add files via upload

---
+++ js practice/main.js
@@ -0,0 +1,77 @@
+const canvas = document.querySelector('canvas')
+const ctx = canvas.getContext('2d')
+
+
+const width = canvas.width = window.innerWidth
+const height = canvas.height = window.innerHeight
+
+function random(min, max) {
+  const num = Math.floor(Math.random() * (max - min + 1)) + min;
+  return num;
+}
+
+function Ball(x, y, velX, velY, color, size) {
+  this.x = x
+  this.y = y
+  this.velX = velX
+  this.velY = velY
+  this.color = color
+  this.size = size
+}
+
+Ball.prototype.draw = function() {
+  ctx.beginPath();
+  ctx.fillStyle = this.color;
+  ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+  ctx.fill();
+}
+
+
+
+Ball.prototype.update = function () {
+  if ((this.x + this.size) >= width){
+    this.velX = -(this.velX)
+  }
+  if ((this.x - this.size) <= 0){
+    this.velX = -(this.velX)
+  }
+  if ((this.y + this.size) >= height){
+    this.velY = -(this.velY)
+  }
+  if ((this.y - this.size) <= 0){
+    this.velY = -(this.velY)
+  }
+  this.x += this.velX
+  this.y += this.velY
+
+}
+
+let balls = [];
+
+while(balls.length < 25) {
+  let size = random(10,20)
+  let ball = new Ball(
+    random(0 + size, width - size),
+    random(0 + size, height - size),
+    random(-7,7),
+    random(-7,7),
+    'rgb(' + random(0, 255) + ',' + random(0, 255) + ',' + random(0, 255) + ')',
+    size
+  );
+
+  balls.push(ball)
+}
+
+function loop (){
+  ctx.fillStyle = 'rgba(0, 0, 0, 0.3)';
+  ctx.fillRect(0, 0, width, height);
+  for (let i = 0; i < balls.length; i++){
+    balls[i].draw()
+    balls[i].update()
+  }
+
+  requestAnimationFrame(loop);
+}
+
+
+loop();
